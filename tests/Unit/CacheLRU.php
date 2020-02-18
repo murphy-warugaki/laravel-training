7f286a384a7638028f52e935fa4351a1714685ed
murphy-warugaki <nishimoto120@gmail.com>
2020-02-18 10:50:35 +0900

    hoge

--- tests/Unit/CacheLRU.php
+++ tests/Unit/CacheLRU.php
@@ -4,4 +4,17 @@ namespace Tests\Unit;
 
 class CacheLRU
 {
+	public $size = 3;
+
+	/*
+	function __construct($size = null) {
+		//$this->size = $size ?: 3;
+
+		if ($size) {
+			$this->size = $size;
+		} else {
+			$this->size = 3;
+		}
+	}
+	*/
 }
