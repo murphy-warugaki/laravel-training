0cb12021c04e1142bba3274a15130fa9e5d0faba
NaotoGon <naotogon5710@gmail.com>
2020-10-23 02:14:29 +0900

    ビューにデータ

--- app/Http/Controllers/SampleController.php
+++ app/Http/Controllers/SampleController.php
@@ -8,6 +8,29 @@ class SampleController extends Controller
 {
     public function index()
     {
-        return view("sample");
+        $fruit_list = [
+            [
+                "name" => "apple",
+                "price" => 100
+            ],
+            [
+                "name" => "banana",
+                "price" => 300
+            ],
+            [
+                "name" => "peach",
+                "price" => 500
+            ],
+        ];
+
+        $total = 0;
+        foreach($fruit_list as $fruit) {
+            $total += $fruit["price"];
+        }
+
+        return view('sample', [
+            "fruit_list" => $fruit_list,
+            "total" => $total
+        ]);
     }
 }
--- resources/views/sample.blade.php
+++ resources/views/sample.blade.php
@@ -1,1 +1,15 @@
 <h2>sample</h2>
+
+<table border="1">
+    <tr>
+        <th>フルーツ名</th>
+    </tr>
+    @foreach($fruit_list as $fruit)
+    <tr>
+        <td>{{$fruit["name"]}}</td>
+    </tr>
+    @endforeach
+</table>
+
+
+<p>合計: {{$total}}</p>
